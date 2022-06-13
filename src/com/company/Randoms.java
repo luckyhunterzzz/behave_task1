254b96d32fe7c1a435195aca366cae6784bd75ec
luckyhunterzzz <lucky_hunter@list.ru>
2022-06-13 19:01:28 +0500

    first commit

---
+++ src/com/company/Randoms.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random = new Random();
+    int min;
+    int max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.ints(min, max + 1).findFirst().getAsInt();
+            }
+        };
+    }
+}
